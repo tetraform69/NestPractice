2104ae8d17ba50b36aa00462a8caa230839dc01a
jhonnCorredor <jhoncorredormedina29@gmail.com>
2024-11-05 15:37:26 -0500

    login

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [LoginController],
   providers: [LoginService],
 })
